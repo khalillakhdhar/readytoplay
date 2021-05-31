b34e3d37919bc78940a5f508d63a8f0fdc0f7aa1
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-31 06:12:57 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/ReadytoplayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadytoplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadytoplayApplication.class, args);
+	}
+
+}
