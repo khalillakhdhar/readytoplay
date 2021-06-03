b9c8fa713c9c01ed72d51df7131120f13d63c682
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-06-03 05:19:32 +0100

    Update ReadytoplayApplication.java

--- src/main/java/com/example/demo/ReadytoplayApplication.java
+++ src/main/java/com/example/demo/ReadytoplayApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ReadytoplayApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ReadytoplayApplication.class, args);
 	}
